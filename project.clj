cbd4b07413f95bc0645f7b3dafef5ce8de50ec6b
logesh-sahaj <logeshb@sahaj.ai>
2023-11-03 18:23:55 +0530

    Logesh | Fix jepsen test failing in Mac M1

--- project.clj
+++ project.clj
@@ -4,6 +4,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main bigo.main
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [jepsen "0.2.1-SNAPSHOT"]
+                 [jepsen "0.2.7":exclusions [net.java.dev.jna/jna
+                                             net.java.dev.jna/jna-platform]]
+                 [net.java.dev.jna/jna "5.13.0"]
+                 [net.java.dev.jna/jna-platform "5.13.0"]
                  [cheshire "5.6.3"]
                  [clj-http "3.12.3"]])
